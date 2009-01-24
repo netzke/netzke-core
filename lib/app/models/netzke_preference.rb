3b700a953f7ad26d160b71fb08190ef2e2eea421
Roman Heinrich <roman.heinrich@gmail.com>
2009-01-24 23:34:28 +0100

    small refactoring in case clause

--- lib/app/models/netzke_preference.rb
+++ lib/app/models/netzke_preference.rb
@@ -29,17 +29,14 @@ class NetzkePreference < ActiveRecord::Base
   end
   
   def normalized_value
-    klass = read_attribute(:pref_type)
+    klass      = read_attribute(:pref_type)
     norm_value = read_attribute(:value)
-    if klass.nil?
-      # do not cast
-      r = norm_value
-    elsif klass == 'Boolean'
-      r = norm_value == 'false' ? false : (norm_value == 'true' || norm_value)
-    elsif klass == 'NilClass'
-      r = nil
-    elsif klass == 'Array' || klass == 'Hash'
-      r = JSON.parse(norm_value)
+    
+    case klass
+    when nil             then r = norm_value  # do not cast
+    when 'Boolean'       then r = norm_value == 'false' ? false : (norm_value == 'true' || norm_value)
+    when 'NilClass'      then r = nil
+    when 'Array', 'Hash' then r = JSON.parse(norm_value)
     else
       r = norm_value.send(ELEMENTARY_CONVERTION_METHODS[klass])
     end
@@ -49,28 +46,25 @@ class NetzkePreference < ActiveRecord::Base
   def normalized_value=(new_value)
     # norm_value = (new_value.to_s if new_value == true or new_value == false) || new_value
     case new_value.class.name
-    when "Array"
-      write_attribute(:value, new_value.to_json)
-    when "Hash"
-      write_attribute(:value, new_value.to_json)
-    else
-      write_attribute(:value, new_value.to_s)
+    when "Array" then write_attribute(:value, new_value.to_json)
+    when "Hash"  then write_attribute(:value, new_value.to_json)
+    else              write_attribute(:value, new_value.to_s)
     end
     write_attribute(:pref_type, [TrueClass, FalseClass].include?(new_value.class) ? 'Boolean' : new_value.class.to_s)
   end
   
   def self.[](pref_name)
-    pref_name = pref_name.to_s
+    pref_name  = pref_name.to_s
     conditions = {:name => pref_name, :user_id => user_id, :widget_name => self.widget_name}
-    pref = self.find(:first, :conditions => conditions)
+    pref       = self.find(:first, :conditions => conditions)
     # pref = @@user.nil? ? self.find_by_name(pref_name) : self.find_by_name_and_user_id(pref_name, @@user.id)
     pref && pref.normalized_value
   end
   
   def self.[]=(pref_name, new_value)
-    pref_name = pref_name.to_s
+    pref_name  = pref_name.to_s
     conditions = {:name => pref_name, :user_id => user_id, :widget_name => self.widget_name}
-    pref = self.find(:first, :conditions => conditions)
+    pref       = self.find(:first, :conditions => conditions)
     
     # if assigning nil, simply delete the eventually found preference
     if new_value.nil?
