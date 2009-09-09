5476ebcbe50c0c53cab6b6dce5294d310a826d60
Sergei Kozlov <s.n.kozlov@gmail.com>
2009-09-08 20:10:17 -0500

    Fix: the API call (JavaScript side) was ignoring the callback parameter.

--- javascripts/core.js
+++ javascripts/core.js
@@ -126,9 +126,13 @@ Ext.widgetMixIn = {
       // this.getChildWidget(params.id).ownerCt.enable();
       
       // provide the callback to that widget that was loading the child, passing the child itself
-      if (this.callbackHash[params.id]) {
-        this.callbackHash[params.id].call(params.scope || this, this.getChildWidget(params.id));
-        delete this.callbackHash[params.id];
+      console.info(this.callbackHash);
+      console.info(params.id);
+      var callbackFn = this.callbackHash[params.id.camelize(true)];
+      if (callbackFn) {
+        callbackFn.call(params.scope || this, this.getChildWidget(params.id));
+        // delete this.callbackHash[params.id];
+        delete callbackFn;
       }
     }
   },
