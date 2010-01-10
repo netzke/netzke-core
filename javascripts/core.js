ec8c4f6cdf047d2b29a898d0bb5da4cd4f0d0151
skozlov <sergei@writelesscode.com>
2010-01-10 12:24:51 -0600

    Ext 3.1.0 compatibility

--- javascripts/core.js
+++ javascripts/core.js
@@ -3,7 +3,7 @@ This file gets loaded along with the rest of Ext library at the initial load
 */
 
 // Check Ext JS version
-var requiredExtVersion = "3.0.3";
+var requiredExtVersion = "3.1.0";
 var currentExtVersion = Ext.version;
 if (requiredExtVersion !== currentExtVersion) {
   alert("Netzke needs Ext " + requiredExtVersion + ". You have " + currentExtVersion + ".");
@@ -574,4 +574,36 @@ Ext.override(Ext.Container, {
       this.doLayout();
     }
   }
+});
+
+
+// Feedback Ghost
+Netzke.FeedbackGhost = function(){};
+Ext.apply(Netzke.FeedbackGhost.prototype, {
+  showFeedback: function(msg){
+    var createBox = function(s, l){
+        return ['<div class="msg">',
+                '<div class="x-box-tl"><div class="x-box-tr"><div class="x-box-tc"></div></div></div>',
+                '<div class="x-box-ml"><div class="x-box-mr"><div class="x-box-mc">', s, '</div></div></div>',
+                '<div class="x-box-bl"><div class="x-box-br"><div class="x-box-bc"></div></div></div>',
+                '</div>'].join('');
+    }
+
+    var showBox = function(msg, lvl){
+      if (!lvl) {lvl = 'notice'};
+      var msgCt = Ext.DomHelper.insertFirst(document.body, {'class':'netzke-feedback'}, true);
+      var m = Ext.DomHelper.append(msgCt, {html:createBox(msg,lvl)}, true);
+      m.slideIn('t').pause(2).ghost("b", {remove:true});
+    }
+
+    if (typeof msg != 'string') {
+      var compoundMsg = "";
+      Ext.each(msg, function(m){
+        compoundMsg += m.msg + '<br>';
+      });
+      if (compoundMsg != "") showBox(compoundMsg, null); // the second parameter will be level
+    } else {
+      showBox(msg);
+    }
+	}
 });
