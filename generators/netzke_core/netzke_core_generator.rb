476806bfa16a5fc12859c148a1270029a95201ec
skozlov <sergei@writelesscode.com>
2010-06-02 20:50:36 +0200

    cleaner migration generator

--- generators/netzke_core/netzke_core_generator.rb
+++ generators/netzke_core/netzke_core_generator.rb
@@ -2,12 +2,9 @@
 class NetzkeCoreGenerator < Rails::Generator::Base
   def manifest
     record do |m|
-      # FIXME: how do we avoid getting the same migration timestamps?
-      # Work-around
-      time = Time.now.utc.strftime("%Y%m%d%H%M%S")
-      m.directory 'db/migrate'
-      # m.file 'create_netzke_layouts.rb', "db/migrate/#{time}_create_netzke_layouts.rb"
-      m.file 'create_netzke_preferences.rb', "db/migrate/#{time.to_i+1}_create_netzke_preferences.rb"
+      m.migration_template 'create_netzke_preferences.rb', 'db/migrate', :assigns => {
+        :migration_name => "CreateNetzkePreferences"
+      }, :migration_file_name => "create_netzke_preferences"
     end
   end
 end
