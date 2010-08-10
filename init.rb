532c0be7d2732c1d0e4f81c3da217151489167d9
Paul Schyska <paul.schyska@familienservice.de>
2010-08-10 18:17:20 +0200

    jsmin requirement fix

--- init.rb
+++ init.rb
@@ -1,1 +1,12 @@
 require 'netzke-core'
+begin
+  require 'jsmin'
+rescue 
+  message=%q(WARNING: netzke-core requires the JSMin gem. You either don't have the gem installed,
+  or you haven't told Rails to require it. If you're using a recent version of Rails:
+  config.gem "jsmin" # in config/environment.rb
+  and of course install the gem: sudo gem install jsmin)
+  puts message
+  Rails.logger.error message
+end
+
