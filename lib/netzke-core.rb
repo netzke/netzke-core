a0f11d4b853238f726b6ac3be4a4e1a9029c2f18
Paul Schyska <paul.schyska@familienservice.de>
2010-08-10 16:55:04 +0200

    enforcing JSMin present on init.

--- lib/netzke-core.rb
+++ lib/netzke-core.rb
@@ -16,6 +16,15 @@ require 'netzke/routing'
   ActiveSupport::Dependencies.load_once_paths.delete(path)
 end
 
+if not defined? JSMin
+  message=%q(WARNING: netzke-core requires the JSMin gem. You either don't have the gem installed,
+or you haven't told Rails to require it. If you're using a recent version of Rails:
+  config.gem "jsmin" # in config/environment.rb
+and of course install the gem: sudo gem install jsmin)
+  puts message
+  Rails.logger.error message
+end
+
 if defined? ActionController
   ActionController::Base.class_eval do
     include Netzke::ControllerExtensions
