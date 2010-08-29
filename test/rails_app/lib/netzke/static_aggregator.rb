32e28ba2634d133052f438e10211d4ff8518f385
Sergei Kozlov <sergei@playcode.nl>
2010-08-29 22:07:39 +0200

    added aggregation example

---
+++ test/rails_app/lib/netzke/static_aggregator.rb
@@ -0,0 +1,38 @@
+module Netzke
+  class StaticAggregator < Widget::Base
+    def default_config
+      super.merge(
+        :ext_config => {
+          :items => [{
+            :region => 'center',
+            :xtype => "netzkeservercaller",
+          }.merge(aggregatee_instance(:center_panel).js_config),{
+            :region => 'west',
+            :xtype => "netzkeextendedservercaller",
+            :width => 300,
+            :split => true
+          }.merge(aggregatee_instance(:west_panel).js_config)]
+          
+        }
+      )
+    end
+    
+    def initial_aggregatees
+      {
+        :center_panel => {
+          :class_name => "ServerCaller"
+        },
+        :west_panel => {
+          :class_name => "ExtendedServerCaller"
+        }
+      }
+    end
+    
+    def self.js_extend_properties
+      {
+        :title => "Static Aggregator",
+        :layout => 'border'
+      }
+    end
+  end
+end
