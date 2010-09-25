17927e1bf834d1e261f839e08e5d88a32d536171
Sergei Kozlov <sergei@playcode.nl>
2010-09-25 21:21:26 +0200

    :lazy_loading config option gets filtered out

--- lib/netzke/component/configuration.rb
+++ lib/netzke/component/configuration.rb
@@ -51,7 +51,7 @@ module Netzke
 
         # Config options that should not go to the client side
         def server_side_config_options
-          []
+          [:lazy_loading]
         end
     
       end
