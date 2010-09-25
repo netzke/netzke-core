e7029156a4d2baa1754b40181e1b8d8a1edf38fe
Sergei Kozlov <sergei@playcode.nl>
2010-09-25 18:13:18 +0200

    Newly generated migration will remove your old netzke_preferences table first.

--- templates/core/create_netzke_preferences.rb
+++ templates/core/create_netzke_preferences.rb
@@ -1,5 +1,7 @@
 class CreateNetzkePreferences < ActiveRecord::Migration
   def self.up
+    drop_table(:netzke_preferences) if table_exists?(:netzke_preferences)
+    
     create_table :netzke_preferences do |t|
       t.string  :key
       t.text    :value
