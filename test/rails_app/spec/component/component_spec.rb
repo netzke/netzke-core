b3f45178749d4b929c8286d53c88c31220913dd5
Mikhail (Alex) Vzorov <alex@playcode.nl>
2011-05-06 17:51:01 +0400

    added rspec test that fails (it succeeds with ExtJS3, though)

---
+++ test/rails_app/spec/component/component_spec.rb
@@ -0,0 +1,20 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+require "netzke-core"
+
+describe "Any component" do
+
+  class TestComponent < Netzke::Base
+
+    endpoint :some_action do
+      {}
+    end
+  end
+
+  it "should be able to register endpoints" do
+    TestComponent.endpoints.should include :some_action
+  end
+
+  it "should have deliver_component endpoint by default" do
+    TestComponent.endpoints.should include :deliver_component
+  end
+end
