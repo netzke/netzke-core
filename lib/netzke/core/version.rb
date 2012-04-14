45d33d05214b4622aaec75922c204d4094a43011
Sergei Kozlov <sergei@playcode.nl>
2012-04-14 16:08:21 +0545

    Update the version

--- lib/netzke/core/version.rb
+++ lib/netzke/core/version.rb
@@ -2,8 +2,8 @@ module Netzke
   module Core
     module Version
       MAJOR = 0
-      MINOR = 7
-      PATCH = 5
+      MINOR = 8
+      PATCH = 0
 
       STRING = [MAJOR, MINOR, PATCH].compact.join('.')
     end
