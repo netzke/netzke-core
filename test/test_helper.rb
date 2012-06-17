a82f1f062b2f4dc112f72e3716fc7833837042e7
Sergey Avseyev <sergey.avseyev@gmail.com>
2012-06-18 00:41:34 +0300

    Make unit tests outside of test app running

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,21 +1,3 @@
-# Set the default environment to sqlite3's in_memory database
-ENV['RAILS_ENV'] ||= 'in_memory'
-
-# Load the Rails environment and testing framework
-require "#{File.dirname(__FILE__)}/app_root/config/environment"
-require 'test_help'
-
-# Undo changes to RAILS_ENV
-silence_warnings {RAILS_ENV = ENV['RAILS_ENV']}
-
-# Run the migrations
-ActiveRecord::Migrator.migrate("#{Rails.root}/db/migrate")
-
-# Set default fixture loading properties
-ActiveSupport::TestCase.class_eval do
-  self.use_transactional_fixtures = true
-  self.use_instantiated_fixtures = false
-  self.fixture_path = "#{File.dirname(__FILE__)}/fixtures"
-
-  fixtures :all
-end
+require 'active_support/all'
+require 'minitest/autorun'
+require 'netzke-core'
