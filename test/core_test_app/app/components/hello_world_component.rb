8cbc272f4c7503996fa2157d3bf07c0cb2efc6a4
Denis Gorin <nmcoder@gmail.com>
2012-10-15 20:48:28 +0200

    Update HelloWorldComponent

--- test/core_test_app/app/components/hello_world_component.rb
+++ test/core_test_app/app/components/hello_world_component.rb
@@ -1,34 +1,27 @@
 class HelloWorldComponent < Netzke::Base
   def configure(c)
+    c.bbar = [:ping_server]
     super
-    # Ext.Panel's config option "title"
-    c.title = "My Hello World Component"
   end
 
-  # Bottom bar with an automatically created action
-  js_property :bbar, [:bug_server]
+  action :ping_server
 
-  # Action to be placed on the bottom bar
-  action :bug_server, :text => 'Greet the World', :icon => :accept
-
-  # Method in the JS class that (by default) processes the action's "click" event
-  js_method :on_bug_server, <<-JS
-    function(){
-      // Remotely calling the server's method greet_the_world (defined below)
-      this.greetTheWorld();
-    }
-  JS
-
-  # Server's method that gets called from the JS
-  endpoint :greet_the_world do |params|
-    # Tell the client side to call its method showGreeting with "Hello World!" as parameter
-    {:show_greeting => "Hello World!"}
+  endpoint :greet_the_world do |params,this|
+    this.show_greeting "Hello World!"
   end
 
-  # Another method in the JS class that gets remotely called by the server side
-  js_method :show_greeting, <<-JS
-    function(greeting){
-      this.body.update("Server says: " + greeting);
-    }
-  JS
+  js_configure do |c|
+    c.on_ping_server = <<-JS
+      function(){
+        // Remotely calling the server's method greet_the_world (defined below)
+        this.greetTheWorld();
+      }
+    JS
+
+    c.show_greeting = <<-JS
+      function(greeting){
+        this.body.update("Server says: " + greeting);
+      }
+    JS
+  end
 end
