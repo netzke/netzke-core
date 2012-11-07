b31e800602553a830dab3fb538d13f178211adf5
nomadcoder <nmcoder@gmail.com>
2012-11-07 16:52:16 +0800

    Add missing gem

--- test/core_test_app/db/schema.rb
+++ test/core_test_app/db/schema.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -17,21 +18,12 @@ ActiveRecord::Schema.define(:version => 20110110132720) do
     t.integer  "user_id"
     t.integer  "role_id"
     t.text     "value"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   add_index "netzke_component_states", ["component"], :name => "index_netzke_component_states_on_component"
   add_index "netzke_component_states", ["role_id"], :name => "index_netzke_component_states_on_role_id"
   add_index "netzke_component_states", ["user_id"], :name => "index_netzke_component_states_on_user_id"
-
-  create_table "netzke_preferences", :force => true do |t|
-    t.string   "key"
-    t.text     "value"
-    t.integer  "user_id"
-    t.integer  "role_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
 
 end
