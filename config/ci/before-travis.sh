62be15a81f1da5f26e539be3c1c9b6af96b93121
nomadcoder <nmcoder@gmail.com>
2012-11-07 14:51:05 +0800

    Try to make travis happy

--- config/ci/before-travis.sh
+++ config/ci/before-travis.sh
@@ -2,10 +2,9 @@
 sh -e /etc/init.d/xvfb start
 
 # fetch extjs
-wget http://cdn.sencha.io/ext-4.0.7-gpl.zip
-unzip -q -d test/core_test_app/public/ -n ext-4.0.7-gpl.zip
-mv test/core_test_app/public/ext-4.0.7-gpl test/core_test_app/public/extjs
+wget http://cdn.sencha.io/ext-4.1.1a-gpl.zip
+unzip -q -d test/core_test_app/public/ -n ext-4.1.1a-gpl.zip
+mv test/core_test_app/public/ext-4.1.1a-gpl test/core_test_app/public/extjs
 
 # cp db configuration
 cp test/core_test_app/config/database.yml.travis  test/core_test_app/config/database.yml
-
