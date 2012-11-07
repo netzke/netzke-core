bc51a6a5c5169827730423e94b91127be5277b0f
nomadcoder <nmcoder@gmail.com>
2012-11-07 16:46:24 +0800

    Another attempt to fix travis

--- config/ci/before-travis.sh
+++ config/ci/before-travis.sh
@@ -4,7 +4,7 @@ sh -e /etc/init.d/xvfb start
 # fetch extjs
 wget http://cdn.sencha.io/ext-4.1.1a-gpl.zip
 unzip -q -d test/core_test_app/public/ -n ext-4.1.1a-gpl.zip
-mv test/core_test_app/public/ext-4.1.1a-gpl test/core_test_app/public/extjs
+mv test/core_test_app/public/ext-4.1.1a test/core_test_app/public/extjs
 
 # cp db configuration
 cp test/core_test_app/config/database.yml.travis  test/core_test_app/config/database.yml
