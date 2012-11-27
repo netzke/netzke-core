bd8433c1f4d5473d29db6332b217475c2f17f372
nomadcoder <nmcoder@gmail.com>
2012-11-27 13:35:50 +0800

    Make child component name accessible by +config.name+

--- lib/netzke/core/component_config.rb
+++ lib/netzke/core/component_config.rb
@@ -1,12 +1,12 @@
 module Netzke::Core
   class ComponentConfig < ActiveSupport::OrderedOptions
     def initialize(name)
-      @name = name.to_s
+      self.name = name.to_s
     end
 
     def set_defaults!
-      self.item_id ||= @name # default item_id
-      self.klass ||= @name.camelize.constantize # default klass
+      self.item_id ||= name # default item_id
+      self.klass ||= name.camelize.constantize # default klass
     end
   end
 end
