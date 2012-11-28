5ac3d66437f61944be1b81a752700a6b8e6617e3
nomadcoder <nmcoder@gmail.com>
2012-11-28 15:56:13 +0800

    Minor impr in Railz::ControllerExtensions

--- lib/netzke/core/railz/controller_extensions.rb
+++ lib/netzke/core/railz/controller_extensions.rb
@@ -1,17 +1,14 @@
 module Netzke
   module Railz
+    # Before each request, Netzke::Base.controller and Netzke::Base.session are set, to be accessible from components.
     module ControllerExtensions
       def self.included(base)
-        base.send(:before_filter, :set_session_data)
-        base.send(:before_filter, :set_controller)
+        base.send(:before_filter, :set_controller_and_session)
       end
 
-      def set_session_data
-        Netzke::Base.session = session
-      end
-
-      def set_controller
+      def set_controller_and_session #:nodoc:
         Netzke::Base.controller = self
+        Netzke::Base.session = session
       end
     end
   end
