2479b842233e9cc7fe50f160918cbee24893e7eb
Max Gorin <nmcoder@gmail.com>
2013-05-13 12:30:55 +0700

    Do not retry Direct requests in test app

--- test/core_test_app/config/initializers/netzke.rb
+++ test/core_test_app/config/initializers/netzke.rb
@@ -1,5 +1,4 @@
 Netzke::Core.setup do |config|
-  config.js_direct_max_retries = 2
   config.ext_uri = "http://cdn.sencha.com/ext-4.1.1a-gpl" if ENV['EXTJS_SRC'] == 'cdn'
 
   # custom session expiration handling
