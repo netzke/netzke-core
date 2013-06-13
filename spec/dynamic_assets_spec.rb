aad198482c14cf5c7e17ff49293fc3030c50a496
Max Gorin <nmcoder@gmail.com>
2013-06-13 09:12:10 +0200

    Disable js comment stripping tests for now

--- spec/dynamic_assets_spec.rb
+++ spec/dynamic_assets_spec.rb
@@ -1,10 +1,11 @@
 require 'spec_helper'
 describe Netzke::Core::DynamicAssets do
-  it "should strip js comments" do
+  # js comment stripping is disabled atm
+  xit "should strip js comments" do
     Netzke::Core::DynamicAssets.strip_js_comments("var test;//commment").should == "var test;"
   end
 
-  it "should not strip // in strings" do
+  xit "should not strip // in strings" do
     Netzke::Core::DynamicAssets.strip_js_comments('var someVar = "abc//def";').should == 'var someVar="abc//def";'
   end
 end
