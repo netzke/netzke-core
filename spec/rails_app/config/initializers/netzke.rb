2f19aaa5c45f5aa0d911c572519ceb162769fc74
Max Gorin <gorinme@gmail.com>
2015-02-19 10:00:18 +0800

    Clean-up old spec helpers

--- spec/rails_app/config/initializers/netzke.rb
+++ spec/rails_app/config/initializers/netzke.rb
@@ -10,6 +10,6 @@ end
 
 ConfigurableOnClassLevel.title = "Overridden"
 
-# Netzke::Testing.setup do |config|
-#   config.spec_root = File.expand_path("../../../../..", __FILE__)
-# end
+Netzke::Testing.setup do |config|
+  config.spec_root = File.expand_path("../../../../..", __FILE__)
+end
