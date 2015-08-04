e6b99d9f396a92cf68263355a586eeaa668af836
Max Gorin <gorinme@gmail.com>
2015-08-04 20:24:08 +0200

    Minor naming fix in specs

--- spec/features/action_config_spec.rb
+++ spec/features/action_config_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 class Foo < Netzke::Base
-  action :foo_action
+  action :foo
 end
 
 class FooExt < Foo
@@ -12,11 +12,11 @@ module Netzke::Core
     it "should preserve localized attributes from superclass if those are not overridden" do
       sc = Foo.new
       esc = FooExt.new
-      sc.actions[:foo_action].text.should == "Foo"
-      esc.actions[:foo_action].text.should == "Foo plus"
+      sc.actions[:foo].text.should == "Foo"
+      esc.actions[:foo].text.should == "Foo plus"
 
-      sc.actions[:foo_action].tooltip.should == "Foo!"
-      esc.actions[:foo_action].tooltip.should == "Foo plus!"
+      sc.actions[:foo].tooltip.should == "Foo!"
+      esc.actions[:foo].tooltip.should == "Foo plus!"
     end
   end
 end
