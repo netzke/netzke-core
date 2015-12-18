486adce2f4e478abcff0ab2f86e524e59bb39242
Max Gorin <max@goodbitlabs.com>
2015-12-18 18:26:56 +0700

    Remove unused asset references (like jquery)

--- spec/rails_app/app/assets/javascripts/application.js
+++ spec/rails_app/app/assets/javascripts/application.js
@@ -10,7 +10,4 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
-//= require turbolinks
 //= require_tree .
